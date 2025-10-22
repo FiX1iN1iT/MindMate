b42c8aa81d1ad2e09bf44a5ecc0f50e5b557b8d4
Alexander <fix1in1it@gmail.com>
2025-10-22 18:19:00 +0300

    init project

---
+++ MindMate/MindMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindMateApp.swift
+//  MindMate
+//
+//  Created by Alexander on 22.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct MindMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
